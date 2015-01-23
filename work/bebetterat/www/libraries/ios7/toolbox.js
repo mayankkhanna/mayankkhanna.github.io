a30e712aa10558ac0b0a3d605ebf2cca7a54521f
Mayank Khanna <mayankk28@gmail.com>
2015-01-23 09:18:59 -0500

    Major revamp to the site, esp. project pages

---
+++ work/bebetterat/www/libraries/ios7/toolbox.js
@@ -0,0 +1,94 @@
+/********************************************************************************************/
+/********************************************************************************************/
+/******************************************       IOS7       ********************************/
+/********************************************************************************************/
+/********************************************************************************************/
+
+var components = {
+	items : {
+		basic : {
+			title :'Basic',
+			items : [ prx.components.text, prx.components.richtext,
+					prx.components.rectangle, prx.components.circle, prx.components.horizontalline,
+					prx.components.verticalline, prx.components.actionarea, prx.components.animationtarget,
+					prx.components.image, prx.components.icon, prx.components.placeholder, prx.components.webview,
+					prx.components.html, prx.components.audio, prx.components.video,
+					prx.components.tooltip
+			]
+		},
+		toolbars : {
+			title :'Toolbars',
+			items : [ 
+			         	prx.components.ios7_navbar, 
+			         	prx.components.ios7_navbar_buttons, 
+			         	prx.components.basic_tabbar_retina,
+			         	prx.components.ios7_tabbar,
+			         	prx.components.ios7_toolbar,
+			         	prx.components.ios7_statusbar
+			         	
+			        ]
+		},
+		buttons: {
+			title: 'Buttons',
+			items: [
+						prx.components.ios7_button_text,
+						prx.components.ios7_button,
+						prx.components.ios7_button_bg,
+						prx.components.ios7_button_icon,
+						prx.components.ios7_button_icon_circular,
+						prx.components.ios7_button_icon_square,
+						prx.components.ios7_button_ui,
+						prx.components.ios7_segmentedcontrol,
+						prx.components.ios7_pagecontroller
+			        ]
+		},
+		forms: {
+			title: 'Forms',
+			items: [
+			        	prx.components.ios7_label,
+						prx.components.ios7_textfield,
+						prx.components.ios7_passwordfield,
+						prx.components.ios7_textarea,
+						prx.components.ios7_checkbox,
+						prx.components.ios7_switch,
+						prx.components.ios7_slider,
+						prx.components.ios7_picker,
+						prx.components.ios8_searchbar
+			]
+		},
+		lists : {
+			title :'Lists',
+			items : [ prx.components.ios7_listbasic, 
+			        prx.components.ios7_listnested,
+					prx.components.ios7_listwithicon, 
+					prx.components.ios7_listwithvalue,
+					prx.components.ios7_listnestedwithvalue,
+					prx.components.ios7_listwithiconandvalue,
+					prx.components.ios7_listwithbadge,
+					prx.components.ios7_listnestedwithbadge,
+					prx.components.ios7_listwithiconandbadge,
+					prx.components.ios7_listradio,
+					prx.components.ios7_listcheckbox,
+					prx.components.ios7_listcomplex,
+					prx.components.ios7_list_header
+			]
+		},
+		other: {
+			title: 'Other Controls',
+			items: [
+						prx.components.ios7_alert,
+						prx.components.ios7_actionsheet,
+						prx.components.ios7_actionsheet_title,
+						prx.components.ios7_control_popover,
+						prx.components.ios7_popover,
+						prx.components.ios7_progressbar,
+						prx.components.ios7_badge,
+						prx.components.ios8_map_tag,
+						prx.components.ios8_message_bubble
+			]
+		}
+		
+		
+	},
+	otherProperties :'insert here'
+};
